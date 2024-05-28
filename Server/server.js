1a67ea6f0253f622bf22e46a23c08c14153b0057
NikhilRaj060 <nikhilraj2061@gmail.com>
2024-05-29 02:29:52 +0530

    updated server.js

--- Server/server.js
+++ Server/server.js
@@ -11,15 +11,14 @@ const app = express();
 app.use(express.json())
 
 app.use(cors({
-    "origin" : `${process.env.FRONTEND_URL}`,
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    origin: `${process.env.FRONTEND_URL}`,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 }));
 
+app.use("/auth/v1", authRoutes)
+app.use("/api/v1", quizRoutes)
 
-app.use("/auth/v1",authRoutes)
-app.use("/api/v1",quizRoutes)
-
-app.use((error, req, res , next) => {
+app.use((error, req, res, next) => {
   console.error(error)
   res.status(500).json({ errorMessage: "Something went wrong" });
 })
@@ -27,10 +26,10 @@ app.use((error, req, res , next) => {
 // Serve static files
 app.use(express.static(path.join(__dirname, "../Client/build")));
 
-app.get("/*",function (req,res) {
+app.get("/*", function (req, res) {
     res.sendFile(
-        path.join(__dirname, "../Client/build"),
-        function(err) {
+        path.join(__dirname, "../Client/build", "index.html"),
+        function (err) {
             if (err) {
                 res.status(500).send(err)
             }
@@ -39,10 +38,10 @@ app.get("/*",function (req,res) {
 })
 
 const PORT = process.env.PORT || 3002;
-connectDB().then(()=>{
-  app.listen(process.env.PORT,()=>{
+connectDB().then(() => {
+  app.listen(PORT, () => {
         console.log(`Server is running at port ${PORT}`)
     })
-}).catch((error)=>{
-    console.error("Error while server is getting up",error)
+}).catch((error) => {
+    console.error("Error while server is getting up", error)
 })
